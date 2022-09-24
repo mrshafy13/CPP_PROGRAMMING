28be50ddc2386b6c1e43747757c6263983aec037
Muhibur Shafy <shafy1559@gmail.com>
2022-09-24 09:26:31 +0600

    24/09/2022

---
+++ Variable_Scope_and_Data_Types.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int	main()
+{
+    int a = 4;
+    int b = 5;
+    cout<<"the value of a is "<<a<<". The value of b is "<<b<<endl;
+    return 0;
+}
---
+++ Variables_and_Comments.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//this is about variable and comments
+//this is a single line comment
+/*this
+is a 
+multi line
+comment
+*/
+int main()
+{
+    int sum = 6;
+    cout<<"Hello World"<<sum;
+    return 0;
+}
