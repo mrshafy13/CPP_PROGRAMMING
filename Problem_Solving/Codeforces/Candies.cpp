80d37be1786d544d6dad803317c22240ef0fce5b
Muhibur Shafy <shafy1559@gmail.com>
2023-05-19 12:03:29 +0600

    19/05/2023

--- Problem_Solving/Codeforces/Candies.cpp
+++ Problem_Solving/Codeforces/Candies.cpp
@@ -2,24 +2,6 @@
 
 using namespace std;
 
-int isPrime(long long n)
-{
-    if(n<=1)
-    {
-        return 1;
-    }
-    else
-    {
-        for(int i = 2; i<= sqrt(n); i++)
-        {
-            if(n%i == 0)
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
 
 int main()
 {
@@ -28,52 +10,17 @@ int main()
     
     while(test_cases--)
     {
-        long long number_of_candies, x;
+        long long number_of_candies, x = 1, r = 2;
         cin >> number_of_candies;
-        int flag = 0;
-        long long result;
-        if(isPrime(number_of_candies) == 0)
+        for(int i = 2;;i++)
         {
-            cout << "1" << endl;
-        }
-        else if(number_of_candies % 3 == 0)
-        {
-            flag = 0;
-            x = number_of_candies/3;
-        }
-        else if(number_of_candies % 2 == 0)
-        {
-            flag = 1;
-            x = number_of_candies/3;
-        }
-        if(flag == 0)
-        {
-            for(x = x; x>=1;)
+            x = number_of_candies/(pow(2,i)-1);
+            long long checker;
+            checker = x*(pow(2,i)-1);
+            if(checker == number_of_candies)
             {
-                if(number_of_candies%x == 0)
-                {
-                    cout << x << endl;
-                    break;
-                }
-                else
-                {
-                    x = x/3;
-                }
-            }
-        }
-        else if(flag == 1)
-        {
-            for(x  = x; x >= 2;)
-            {
-                if(number_of_candies % x == 0)
-                {
-                    cout << x << endl;
-                    break;
-                }
-                else
-                {
-                    x = x/2;
-                }
+                cout << x << endl;
+                break;
             }
         }
     }
